56b16081d0a1efe9fb9c5a18e35b3adf11cc13da
Greg Ziegan <greg.ziegan@gmail.com>
2014-09-17 10:23:12 -0400

    added storytoken model, cleaned up api

--- anovelmous/__init__.py
+++ anovelmous/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, redirect, g, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 import flask_restless
+from sqlalchemy import UniqueConstraint, ForeignKeyConstraint
 
 import datetime
 
@@ -15,7 +16,7 @@ class Novel(db.Model):
     name = db.Column(db.Unicode, unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, name=None):
+    def __init__(self, name):
         self.name = name
         self.created_at = datetime.datetime.utcnow()
 
@@ -27,7 +28,7 @@ class Chapter(db.Model):
     novel_id = db.Column(db.Integer, db.ForeignKey('novel.id'), nullable=False)
     novel = db.relationship('Novel', backref=db.backref('chapters', lazy='dynamic'))
 
-    def __init__(self, name=None, novel_id=None):
+    def __init__(self, name, novel_id):
         self.name = name
         self.created_at = datetime.datetime.utcnow()
         self.novel_id = novel_id
@@ -35,28 +36,60 @@ class Chapter(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Unicode, unique=True, nullable=False)
-    first_name = db.Column(db.Unicode, nullable=False)
-    last_name = db.Column(db.Unicode, nullable=False)
+    username = db.Column(db.Unicode, primary_key=True)
     password = db.Column(db.String, nullable=False)
     is_active = db.Column(db.Boolean, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, username=None, first_name=None, last_name=None, password=None, is_active=True):
+    """__table_args__ = (
+        UniqueConstraint("id", "username"),
+    )"""
+
+    def __init__(self, username, password, is_active=True):
         self.username = username
-        self.first_name = first_name
-        self.last_name = last_name
         self.password = password
         self.is_active = is_active
+        self.created_at = datetime.datetime.utcnow()
+
 
 class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.Unicode, nullable=False)
+    token = db.Column(db.Unicode, nullable=False)
+    is_punctuation = db.Column(db.Boolean ,nullable=False)
     ordinal = db.Column(db.Integer, nullable=False)
     selected = db.Column(db.Boolean, nullable=False)
     chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
     chapter = db.relationship('Chapter', backref=db.backref('content', lazy='dynamic'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('votes', lazy='dynamic'))
+    username = db.Column(db.Unicode, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    ForeignKeyConstraint(['user_id', 'username'], ['user.id', 'user.username'])
+
+    def __init__(self, token, is_punctuation, ordinal, selected, chapter_id, username, user_id):
+        self.token = token
+        self.is_punctuation = is_punctuation
+        self.ordinal = ordinal
+        self.selected = selected
+        self.chapter_id = chapter_id
+        self.username = username
+        self.user_id = user_id
+        self.created_at = datetime.datetime.utcnow()
+
+
+class StoryToken(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.Unicode, nullable=False)
+    is_punctuation = db.Column(db.Boolean, nullable=False)
+    ordinal = db.Column(db.Integer, nullable=False)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token, is_punctuation, ordinal, chapter_id):
+        self.token = token
+        self.is_punctuation = is_punctuation
+        self.ordinal = ordinal
+        self.chapter_id = chapter_id
+        self.created_at = datetime.datetime.utcnow()
 
 
 @app.route('/')
@@ -79,7 +112,7 @@ def read_novel(novel_name):
         .order_by(Chapter.created_at).first()
     if not most_recent_chapter:
         abort(404)
-    chapter_content = Vote.query.filter_by(chapter_id=most_recent_chapter.id).all()
+    chapter_content = StoryToken.query.filter_by(chapter_id=most_recent_chapter.id).all()
 
     template_variables = {
         'novel': novel,
@@ -92,8 +125,10 @@ def read_novel(novel_name):
 manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
 manager.create_api(Novel, methods=['GET', 'POST'])
 manager.create_api(Chapter, methods=['GET', 'POST'])
-manager.create_api(User, exclude_columns=['is_active', 'password'], methods=['GET', 'POST'])
+manager.create_api(User, primary_key='username', exclude_columns=['is_active', 'password'], methods=['GET', 'POST'])
 manager.create_api(Vote, methods=['GET', 'POST'])
+manager.create_api(StoryToken, methods=['GET', 'POST'])
+
 
 if __name__ == '__main__':
     db.create_all()
