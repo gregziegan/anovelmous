dd276dadbe1c052fae111af6d9855898ff07ebe6
Greg Ziegan <grz5@case.edu>
2014-12-20 18:30:55 -0500

    new index page

--- anovelmous/__init__.py
+++ anovelmous/__init__.py
@@ -14,9 +14,10 @@ def initialize_database():
 
 @app.route('/')
 def index():
-    novels = Novel.query.all()
-    current_novel = novels[-1] if novels else None
-    return render_template('index.html', current_novel=current_novel)
+    current_novel = Novel.query.order_by('-id').first()
+    current_chapter = Chapter.query.order_by('-id').first()
+    current_chapter_tokens = StoryToken.query.filter_by(chapter_id=current_chapter.id).all()
+    return render_template('index.html', current_novel=current_novel, current_chapter_tokens=current_chapter_tokens)
 
 
 @app.route('/browse')
@@ -51,4 +52,6 @@ manager.create_api(StoryToken, methods=['GET', 'POST'], allow_functions=True)
 
 
 if __name__ == '__main__':
+    if app.debug:
+        db.init_app(app)
     app.run()
