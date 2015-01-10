0eb0e992ce8a899584bbdf52e2d5d58e3fbf72d2
Greg Ziegan <grz5@case.edu>
2015-01-10 11:37:03 -0500

    fixed formatting algorithm

--- anovelmous/formatting.py
+++ anovelmous/formatting.py
@@ -4,32 +4,33 @@ import string
 
 
 def update_formatted_novel_tokens(novel_token, quote_punctuation=None):
-    previous_formatted_token = FormattedNovelToken.query.order_by(desc(FormattedNovelToken.ordinal)).first()
+    prev_formatted_novel_token = FormattedNovelToken.query.order_by(desc(FormattedNovelToken.ordinal)).first()
 
-    if not previous_formatted_token:
-        first_formatted_novel_token = FormattedNovelToken(
-            token=novel_token.token,
-            ordinal=0,
-            chapter_id=novel_token.chapter_id
-        )
-        db.session.add(first_formatted_novel_token)
-        db.session.commit()
-
+    if not prev_formatted_novel_token:
+        create_new_formatted_novel_token(novel_token, first_in_chapter=True)
     elif novel_token.token not in string.punctuation:
-        if previous_formatted_token == '"':
-            previous_formatted_token.token += novel_token.token
-            db.session.add(previous_formatted_token)
-            db.session.commit()
+        if prev_formatted_novel_token == '"':
+            update_previous_formatted_novel_token(novel_token, prev_formatted_novel_token)
         else:
-            new_formatted_novel_token = FormattedNovelToken(
-                token=novel_token.token,
-                ordinal=previous_formatted_token.ordinal+1,
-                chapter_id=novel_token.chapter_id
-            )
-            db.session.add(new_formatted_novel_token)
-            db.session.commit()
-    elif quote_punctuation == 'LEFT':
-        previous_formatted_token.token += novel_token.token
+            create_new_formatted_novel_token(novel_token)
+    elif novel_token.token in string.punctuation and quote_punctuation != 'RIGHT':
+        update_previous_formatted_novel_token(novel_token, prev_formatted_novel_token)
+    elif quote_punctuation == 'RIGHT':
+        create_new_formatted_novel_token(novel_token)
 
-        db.session.add(previous_formatted_token)
-        db.session.commit(previous_formatted_token)
+
+def create_new_formatted_novel_token(novel_token, first_in_chapter=False):
+    ordinal = 0 if first_in_chapter else novel_token.ordinal + 1
+    new_formatted_novel_token = FormattedNovelToken(
+        token=novel_token.token,
+        ordinal=ordinal,
+        chapter_id=novel_token.chapter_id
+    )
+    db.session.add(new_formatted_novel_token)
+    db.session.commit()
+
+
+def update_previous_formatted_novel_token(novel_token, prev_formatted_novel_token):
+    prev_formatted_novel_token.token += novel_token.token
+    db.session.add(prev_formatted_novel_token)
+    db.session.commit()
